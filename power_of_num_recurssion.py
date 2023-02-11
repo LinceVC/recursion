b55b95b5be31cf763c7defdb8c996fa5b6c7a464
Lince V Chandy <61664130+LinceVC@users.noreply.github.com>
2023-02-11 12:34:43 +0530

    power_of_num_using_recurssion
    finding power of a number using recursion

---
+++ power_of_num_recurssion.py
@@ -0,0 +1,15 @@
+## power of given number using recurssion 
+## Author: Lince
+## Date: 11Feb 2023
+
+def power_of_num_rec(base,pwr):
+    if pwr ==0:
+        return 1
+    return base*power_of_num_rec(base,pwr-1)
+
+
+##Input base and power
+base =int(input())
+pwr = int(input())
+
+power_of_num_rec(base,pwr)
